b5e5a8a0ee16d2bbcd7a7983d8a4ae2c30acb7e5
KimGyeongSeok (김경석) <79523251+KimGSeok@users.noreply.github.com>
2024-07-18 23:45:18 +0900

    [level 0] Title: 모스부호 (1), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,14 @@
+const morse = {
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+
+function solution(letter) {
+    const letterArray = letter.split(' ');
+    return letterArray.map((item) => {
+        return morse[item]   
+    }).join('')
+}
